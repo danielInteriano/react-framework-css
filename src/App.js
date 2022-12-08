be4a18b29540a7be9effe910be5a1e625d597e8d
Daniel Interiano <danielstanly@yahoo.es>
2022-12-08 11:13:54 -0600

    Framework Bootstrap and Bulma con React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import MaterialUI from './components/MaterialUI';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h2>Frameworks CSS con React</h2>
+			<hr></hr>
+
+			{/* <Bootstrap></Bootstrap> */}
+			{/* <Bulma></Bulma> */}
+			{/* <ReactBootstrap></ReactBootstrap> */}
+
+			<MaterialUI></MaterialUI>
+			<br />
+			<br />
+		</div>
+	);
 }
 
 export default App;
